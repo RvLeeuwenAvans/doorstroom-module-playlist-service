26479265c508ce99366ee4a8c7eea824cf2ef707
RvLeeuwen <r.vanleeuwen2@student.avans.nl>
2025-01-01 14:50:38 +0100

    Setup AuthenticationController and configure the firewall for user-authentication/ redirect logic

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\LoginFormType;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,12 +11,21 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthenticationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(
+        Request                     $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface      $entityManager
+    ): Response
     {
+        if ($this->getUser() !== null) {
+            return $this->redirectToRoute('app_home');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -23,24 +33,48 @@ class AuthenticationController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var string $username */
             $username = $form->get('username')->getData();
-
-            // set username for user
             $user->setUsername($username);
 
             /** @var string $plainPassword */
             $plainPassword = $form->get('plainPassword')->getData();
-
             // encode the plain password
             $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+            $user->setRoles(['ROLE_USER']);
 
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('authentication/register.html.twig', [
             'registrationForm' => $form,
         ]);
+    }
+
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        if ($this->getUser() !== null) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        $form = $this->createForm(LoginFormType::class);
+        // Handling of the form submission and user authentication is handled
+        // automatically by: FormLoginAuthenticator.php defined in security.yaml.
+
+        // Get the login error if there is one and pass it to the view, to display the message.
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('authentication/login.html.twig', [
+            'loginForm' => $form,
+            'error' => $error
+        ]);
+    }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+    {
+        // Empty body, request intercepted- and redirected by the project firewall.
     }
 }
