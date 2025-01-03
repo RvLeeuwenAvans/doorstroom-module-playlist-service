24a0f904a4805ab7a70e7bfdb3dacdd93e635f0c
RvLeeuwen <r.vanleeuwen2@student.avans.nl>
2025-01-03 13:32:08 +0100

    functioning add song and filter logic.

--- src/Controller/MusicController.php
+++ src/Controller/MusicController.php
@@ -14,37 +14,46 @@ class MusicController extends AbstractController
 {
     #[Route(path: '/music', name: 'app_music')]
     public function addSong(
-        Request                     $request,
-        EntityManagerInterface      $entityManager
+        Request                $request,
+        EntityManagerInterface $entityManager
     ): Response
     {
         $song = new Song();
         $form = $this->createForm(AddSongFormType::class, $song);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var string $name */
-            $name = $form->get('name')->getData();
-            $song->setName($name);
-            /** @var string $band */
-            $band = $form->get('band')->getData();
-            $song->setBand($band);
-            /** @var string $genre */
-            $genre = $form->get('genre')->getData();
-            $song->setGenre($genre);
-            /** @var string $link */
-            $link = $form->get('link')->getData();
-            $song->setLink($link);
+        $filter = $request->get("genre") ?: "everything";
 
-            $entityManager->persist($song);
-            $entityManager->flush();
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                /** @var string $name */
+                $name = $form->get('name')->getData();
+                $song->setName($name);
+                /** @var string $band */
+                $band = $form->get('band')->getData();
+                $song->setBand($band);
+                /** @var string $genre */
+                $genre = $form->get('genre')->getData();
+                $song->setGenre($genre);
+                /** @var string $link */
+                $link = $form->get('link')->getData();
+                $song->setLink($link);
+
+                $entityManager->persist($song);
+                $entityManager->flush();
+            }
         }
 
         $songCatalog = $entityManager->getRepository(Song::class)->findAll();
+        // todo: make separate table for genres, get all genres here and inject intro template
+        // alter music form to make genres a select box
+//        $genres = ded
 
         return $this->render('music.html.twig', [
             'addMusicForm' => $form,
             'songs' => $songCatalog,
+            'filter' => $filter
         ]);
     }
 }
