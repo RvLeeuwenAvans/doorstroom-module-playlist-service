ac86e787c17e92456a320f546ebc8a15d2f28168
RvLeeuwen <r.vanleeuwen2@student.avans.nl>
2025-01-05 18:49:32 +0100

    cleanup HTML and CSS to make it validatable in a compiled state.

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -56,7 +56,7 @@ class AuthenticationController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser() !== null) {
-            return $this->redirectToRoute('app_music');
+            return $this->redirectToRoute('app_music_catalog');
         }
 
         // Handling of the form submission and user authentication is handled
